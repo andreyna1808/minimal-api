dc96f632891d8dbc4f23df3d43d518fe3d924a2c
Andreyna Carvalho <andreyna.m.carvalho@gmail.com>
2025-08-10 07:13:27 -0400

    minimal api

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using minimal_api.Dominio.DTOs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona serviços do Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login realizado com sucesso!");
+    }
+
+    return Results.Unauthorized();
+});
+
+app.Run();
+
